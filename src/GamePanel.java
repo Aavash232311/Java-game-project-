e8095e4c13d36054ffb40f6d666498506b7f8953
Avash <aavash2005@gmail.com>
2025-10-20 21:02:03 -0500

    turn even smoother

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -309,6 +309,10 @@ class Frame {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+//            for (Point point: markerPoint) {
+//                g.setColor(Color.RED);
+//                g.drawRect(point.x, point.y, stdSize, stdSize);
+//            }
 //            g.drawRect(initX, initY, stdSize, stdSize); // this is the box of the character.
             if (moveable.contains(new Point(initX, initY))) {
                 lastGrid = new Point(initX, initY);
@@ -408,11 +412,26 @@ class Frame {
             | 2         | 1           | 0           | Left          |
             | 3         | 0           | 1           | Down          |
          */
+
+
+        private Point backRange(int backLoop, int x, int y) { // sometimes we click when we are at the middle and it seems laggy so we want make the initial point little back ward
+            int count = backLoop;
+
+            while (count >= 0) {
+                x -= vectorX[direction];
+                y -= vectorY[direction];
+                count--;
+            }
+            return new Point(x, y);
+        }
+
+//        ArrayList<Point> markerPoint = new ArrayList<>();
         private void vectorChange(int vectorChangeMag) {
             /* In our earlier code we did things in the "hard" way. Now what can we do is first,
             * get the block in which we are, loop till 5 blocks example if we found nearest block earlier then we can simply skip the cost function part. */
             Point currentCharacterPoint = new Point(initX, initY);
-            int reqX = currentCharacterPoint.x, reqY = currentCharacterPoint.y, count = 0;
+            Point calibratedBack = backRange(3, currentCharacterPoint.x, currentCharacterPoint.y);
+            int reqX = calibratedBack.x, reqY = calibratedBack.y, count = 0;
 
             Point turnPoint = null;
             Point turnTo = null;
@@ -423,6 +442,7 @@ class Frame {
                         break;
                     }
                     Point testPoint = new Point(reqX, reqY);
+//                    markerPoint.add(new Point(reqX, reqY));
                     testPoint.x += vectorX[vectorChangeMag];
                     testPoint.y += vectorY[vectorChangeMag];
                     if (moveable.contains(testPoint)) {
